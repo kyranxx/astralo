c067e6205d90389f06276c7f4913850e3ac77497
kyranxx <blanarikdaniel@gmail.com>
2025-12-05 14:01:26 +0100

    Switch to @astrojs/vercel adapter for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   trailingSlash: 'ignore',
   build: {
     format: 'file'
